5ce5734da178a502f474de0a2444a744047ed0cc
AHMED ABDULLAH ALGHARIB <ahmed.algharib42@gmail.com>
2025-03-11 08:56:46 -0700

    New

--- 01_C++_Basic/02_Week2_(Basic)/Assignments/Assignments7_display_pattern_diamond.cpp
+++ 01_C++_Basic/02_Week2_(Basic)/Assignments/Assignments7_display_pattern_diamond.cpp
@@ -5,6 +5,31 @@ using namespace std;
 int main()
 {
     int n;
-
+    cout << "Enter the number of rows(half of the hieght diamond): ";
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n - i; ++j)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; ++j)
+        {
+            cout << "*";
+        }
+        cout<<endl;
+    }
+    for (int i = n - 1; i >= 1; --i)
+    {
+        for (int j = 1; j <= n - i; ++j)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i-1 ; ++j)
+        {
+            cout << "*";
+        }
+        cout<<endl;
+    }
     return 0;
 }
